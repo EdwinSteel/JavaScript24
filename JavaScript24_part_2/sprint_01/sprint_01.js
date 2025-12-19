25946de7340bb72e540c51b4ae8916ee1499f133
NicPH <admslavmkl@gmail.com>
2025-12-19 14:35:42 +0200

    sp01 t 02

--- JavaScript24_part_2/sprint_01/sprint_01.js
+++ JavaScript24_part_2/sprint_01/sprint_01.js
@@ -11,6 +11,8 @@ console.log(arr_01, typeof arr_01);
 // Task 02
 // Создайте массив arr_02, который содержит два элемента - true, false. Выведите в консоль. 
 
+let arr_02 = [true, false];
+console.log(arr_02);
 
 // Task 03
 // Создайте массив arr_03 содержащий элементы  "Debian", 10, "Buster". Выведите в консоль. 

59101e04d28e60003271c75f8c16c97ae66f5b4b
NicPH <admslavmkl@gmail.com>
2025-12-19 14:20:37 +0200

    sp01 t 01

--- JavaScript24_part_2/sprint_01/sprint_01.js
+++ JavaScript24_part_2/sprint_01/sprint_01.js
@@ -293,3 +293,32 @@ const t25 = (arr) => {
 document.querySelector('.b-25').addEventListener('click', () => {
    document.querySelector('.out-25').textContent =  t25([44, 55]);
 });
+
+// ***********************************************************************
+// iTGid.info - курс JS24/2
+// Task 01
+// Task 02
+// Task 03
+// Task 04
+// Task 05
+// Task 06
+// Task 07
+// Task 08
+// Task 09
+// Task 10
+// Task 11
+// Task 12
+// Task 13
+// Task 14
+// Task 15
+// Task 16
+// Task 17
+// Task 18
+// Task 19
+// Task 20
+// Task 21
+// Task 22
+// Task 23
+// Task 24
+// Task 25
+// -- ok

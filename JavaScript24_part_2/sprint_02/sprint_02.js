80ea0363ac6bf62bcc2b8a0b3759893ddcadd24e
NicPH <admslavmkl@gmail.com>
2026-01-28 14:52:35 +0200

    sp02 start

--- JavaScript24_part_2/sprint_02/sprint_02.js
+++ JavaScript24_part_2/sprint_02/sprint_02.js
@@ -325,3 +325,31 @@ const t25 = () => {
 }
 
 document.querySelector('.b-25').addEventListener('click', t25);
+
+// *******************************************
+// Task 01
+// Task 02
+// Task 03
+// Task 04
+// Task 05
+// Task 06
+// Task 07
+// Task 08
+// Task 09
+// Task 10
+// Task 11
+// Task 12
+// Task 13
+// Task 14
+// Task 15
+// Task 16
+// Task 17
+// Task 18
+// Task 19
+// Task 20
+// Task 21
+// Task 22
+// Task 23
+// Task 24
+// Task 25
+// -- ok
